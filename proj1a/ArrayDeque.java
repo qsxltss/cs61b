7330a1b8d19f5273926076be431ff48dae355f3c
qsxltss <2000012913@stu.pku.edu.cn>
2023-02-17 18:28:49 +0800

    proj1A ver1 down

---
+++ proj1a/ArrayDeque.java
@@ -0,0 +1,104 @@
+public class ArrayDeque <T>{
+    private int len;
+    private T[] arr;
+
+    public ArrayDeque()
+    {
+        arr = (T[])new Object[8];
+        len = 0;
+    }
+    public ArrayDeque(ArrayDeque other)
+    {
+        arr = (T[]) new Object[other.size()];
+        for(int i= 0; i< other.size();i++)
+        {
+            arr[i] =(T)other.arr[i];
+        }
+        len = other.size();
+    }
+    public void addFirst(T item)
+    {
+        T[] arr_new = (T[])new Object[len+1];
+        arr_new[0] = item;
+        for(int i = 0; i < len; i++)
+        {
+            arr_new[i+1] = arr[i];
+        }
+        arr = arr_new;
+        len += 1;
+    }
+    public void addLast(T item)
+    {
+        T[] arr_new = (T[])new Object[len+1];
+        arr_new[len] = item;
+        for(int i = 0; i < len; i++)
+        {
+            arr_new[i] = arr[i];
+        }
+        arr = arr_new;
+        len+=1;
+    }
+    public boolean isEmpty()
+    {
+        if(len == 0 ) return true;
+        return false;
+    }
+    public int size()
+    {
+        return len;
+    }
+    public void printDeque()
+    {
+        /* 没有内容*/
+        if(len == 0)
+        {
+            System.out.println("len = 0!");
+        }
+        for(int i = 0; i < len-1; i++)
+        {
+            System.out.print(arr[i]+" ");
+        }
+        System.out.print(arr[len-1]);
+        System.out.println();
+    }
+    public T removeFirst()
+    {
+        /* 没有内容*/
+        if(len == 0)
+        {
+            System.out.println("len = 0!");
+        }
+
+        T x = arr[0];
+        T[] arr_new = (T[])new Object[len-1];
+        for(int i = 0; i < (len-1); i++)
+        {
+            arr_new[i] = arr[i+1];
+        }
+        arr = arr_new;
+        len -= 1;
+        return x;
+    }
+    public T removeLast()
+    {
+        /* 没有内容*/
+        if(len == 0)
+        {
+            System.out.println("len = 0!");
+        }
+
+        T x = arr[len-1];
+        T[] arr_new = (T[])new Object[len-1];
+        for(int i = 0; i < (len-1); i++)
+        {
+            arr_new[i] = arr[i];
+        }
+        arr = arr_new;
+        len -= 1;
+        return x;
+    }
+    public T get(int index)
+    {
+        return arr[index];
+    }
+}
---
+++ proj1a/LinkedListDeque.java
@@ -0,0 +1,152 @@
+public class LinkedListDeque <Type> {
+    private node sentinel;
+    //private node last;
+    private int len;
+    private class node
+    {
+        public Type item;
+        public node next;
+        public node prev;
+        public node(Type x, node p, node n)
+        {
+            item = x;
+            next = n;
+            prev = p;
+        }
+    }
+    public LinkedListDeque()
+    {
+        sentinel = new node(null,null,null);
+        sentinel.prev = sentinel;
+        sentinel.next = sentinel;
+        len = 0;
+    }
+    public LinkedListDeque(LinkedListDeque other)
+    {
+        sentinel = new node(null,null,null);
+        sentinel.prev = sentinel;
+        sentinel.next = sentinel;
+        len =0;
+        node copy = other.sentinel;
+        for(int i=0; i< other.len; i++)
+        {
+            copy = copy.next;
+            this.addLast(copy.item);
+        }
+    }
+    public void addFirst(Type item)
+    {
+        node x = new node(item,sentinel,sentinel.next);
+        sentinel.next.prev = x;
+        sentinel.next = x;
+        len++;
+    }
+    public void addLast(Type item)
+    {
+        node x = new node(item,sentinel.prev,sentinel);
+        sentinel.prev.next = x;
+        sentinel.prev = x;
+        len++;
+    }
+    public boolean isEmpty()
+    {
+        if (len == 0) return true;
+        return false;
+    }
+    public int size()
+    {
+        return len;
+    }
+
+    public void printDeque()
+    {
+        /* len=0:不存在node */
+        if(len <= 0)
+        {
+            System.out.println("len is 0!");
+        }
+        node x = sentinel.next;
+        for(int i=0; i<len-1; i++)
+        {
+            System.out.print(x.item+" ");
+            x = x.next;
+        }
+        System.out.print(x.item);
+        System.out.println();
+    }
+
+    public Type removeFirst()
+    {
+        /* len=0:不存在node */
+        if(len == 0)
+        {
+            System.out.println("len is 0!");
+            return null;
+        }
+        node x = sentinel.next;
+        x.next.prev = sentinel;
+        sentinel.next = x.next;
+        len--;
+        return x.item;
+    }
+    public Type removeLast()
+    {
+        /* len=0:不存在node */
+        if(len == 0)
+        {
+            System.out.println("len is 0!");
+            return null;
+        }
+        node x = sentinel.prev;
+        x.prev.next = sentinel;
+        sentinel.prev = x.prev;
+        len--;
+        return x.item;
+    }
+
+    /**/
+    public Type get(int index)
+    {
+        node x = sentinel;
+        /* len=0:不存在node */
+        if(len == 0)
+        {
+            System.out.println("len is 0!");
+            return null;
+        }
+        for(int i = 0; i<len; i++)
+        {
+            x = x.next;
+            if(index == i)
+            {
+                return x.item;
+            }
+        }
+        /* index>=len:超过了能get的长度 */
+        System.out.println("index is too big!");
+        return null;
+    }
+
+    /* helper function for getrecursive */
+    public Type getRecur(node x, int index)
+    {
+        if(index == 0) return x.item;
+        return getRecur(x.next,index-1);
+    }
+    public Type getRecursive(int index)
+    {
+        /* index>=len:超过了能get的长度 */
+        if(index >= len)
+        {
+            System.out.println("index is too big!");
+            return null;
+        }
+        /* len=0:不存在node */
+        if(len == 0)
+        {
+            System.out.println("len is 0!");
+            return null;
+        }
+        return getRecur(sentinel.next,index);
+    }
+}
